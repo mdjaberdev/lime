3b66d8d36e74c347d6c5c0bceba21c02788ea236
mdjaberdev <mdjaber.dev@gmail.com>
2026-07-15 21:05:10 +0600

    responsive header banner

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -3,19 +3,19 @@ import Button from "../Button";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('/src/assets/banner.png')] bg-cover bg-center bg-no-repeat pt-[400px] pb-[250px] text-center">
-      <h1 className="text-7xl text-white font-poppins">Ride Green</h1>
-      <div className="mt-8">
+    <div className="bg-[url('/src/assets/banner.png')] bg-cover bg-center bg-no-repeat pt-52 sm:pt-64 md:pt-72 lg:pt-[400px] pb-24 sm:pb-32 md:pb-40 lg:pb-[250px] text-center px-4">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-poppins leading-tight">
+        Ride Green
+      </h1>
+
+      <div className="mt-8 flex flex-col sm:flex-row justify-center items-center gap-4 lg:block">
         <Button
-          className={
-            "hover:bg-transparent duration-300 inline-block hover:text-white hover:border-white cursor-pointer"
-          }
+          className="hover:bg-transparent duration-300 hover:text-white hover:border-white cursor-pointer  sm:w-auto lg:inline-block"
           btnText={"Our cities"}
         />
+
         <Button
-          className={
-            "hover:bg-transparent duration-300 inline-block hover:text-white hover:border-white cursor-pointer ml-4"
-          }
+          className="hover:bg-transparent duration-300 hover:text-white hover:border-white cursor-pointer  sm:w-auto lg:inline-block lg:ml-4"
           btnText={"Download the App"}
         />
       </div>
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,57 +1,115 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
-import Images from '../Images'
-import logo from '/src/assets/logo.png'
+import React, { useState } from "react";
+import Container from "../Container";
+import Flex from "../Flex";
+import Images from "../Images";
+import logo from "/src/assets/logo.png";
 import langues from "/src/assets/langues.png";
-import { Link } from 'react-router-dom'
-import Button from '../Button'
+import { Link } from "react-router-dom";
+import Button from "../Button";
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="fixed w-full pt-10">
+    <div className="fixed top-0 left-0 w-full pt-4 lg:pt-10 z-50">
       <Container>
-        <Flex className={"justify-between"}>
-          <div className="flex gap-x-6">
-            <Link to={"/"}>
+        {/* Top Header */}
+        <Flex className="items-center justify-between">
+          {/* Logo */}
+          <div className="flex items-center gap-x-4 lg:gap-x-6">
+            <Link to="/">
               <Images srcImg={logo} />
             </Link>
+
             <Link>
               <Images srcImg={langues} />
             </Link>
           </div>
-          <div className="ml-80">
+
+          {/* Desktop Menu (LG Same) */}
+          <div className="hidden lg:block ml-80">
             <ul className="flex gap-x-6 text-sm text-white font-poppins">
               <li>
-                <Link to={"/about-Us"}>About Us</Link>
+                <Link to="/about-Us">About Us</Link>
               </li>
               <li>
-                <Link to={"/why-Lime"}>Why Lime</Link>
+                <Link to="/why-Lime">Why Lime</Link>
               </li>
               <li>
-                <Link to={"/vehicles"}>Vehicles</Link>
+                <Link to="/vehicles">Vehicles</Link>
               </li>
               <li>
-                <Link to={"/locations"}>Locations</Link>
+                <Link to="/locations">Locations</Link>
               </li>
               <li>
-                <Link to={"/advertise"}>Advertise</Link>
+                <Link to="/advertise">Advertise</Link>
               </li>
               <li>
-                <Link to={"/blog"}>Blog</Link>
+                <Link to="/blog">Blog</Link>
               </li>
               <li>
-                <Link to={"/help"}>Help</Link>
+                <Link to="/help">Help</Link>
               </li>
             </ul>
           </div>
-          <div className="">
-            <Button className={"cursor-pointer"} btnText={"Download App"} />
+
+          {/* Desktop Button */}
+          <div className="hidden lg:block">
+            <Button className="cursor-pointer" btnText="Download App" />
           </div>
+
+          {/* Mobile Menu Button */}
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-white  text-4xl lg:hidden"
+          >
+            {menuOpen ? <HiOutlineX /> : <HiOutlineMenuAlt3 />}
+          </button>
         </Flex>
+
+        {/* Mobile Menu */}
+        <div
+          className={`lg:hidden overflow-hidden transition-all duration-300 ${
+            menuOpen ? "max-h-[500px] mt-5" : "max-h-0"
+          }`}
+        >
+          <div className="bg-black/90 backdrop-blur-md rounded-xl p-6">
+            <ul className="flex flex-col gap-5 text-white font-poppins text-center">
+              <li>
+                <Link to="/about-Us">About Us</Link>
+              </li>
+              <li>
+                <Link to="/why-Lime">Why Lime</Link>
+              </li>
+              <li>
+                <Link to="/vehicles">Vehicles</Link>
+              </li>
+              <li>
+                <Link to="/locations">Locations</Link>
+              </li>
+              <li>
+                <Link to="/advertise">Advertise</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link to="/help">Help</Link>
+              </li>
+            </ul>
+
+            <div className="mt-6">
+              <Button
+                className="w-full cursor-pointer"
+                btnText="Download App"
+              />
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
